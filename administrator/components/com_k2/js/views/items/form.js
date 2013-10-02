e7ccddd0fc5192d8919f0c78b85421d961a19082
Lefteris Kavadas <lefteris@Lefteriss-iMac.local>
2013-10-02 17:47:42 +0300

    Added the basic events and functions for future implementation of the tags autocomplete in item form.

--- administrator/components/com_k2/js/views/items/form.js
+++ administrator/components/com_k2/js/views/items/form.js
@@ -1,14 +1,18 @@
 'use strict';
-define(['marionette', 'text!layouts/items/form.html', 'dispatcher'], function(Marionette, template, K2Dispatcher) {
+define(['marionette', 'text!layouts/items/form.html', 'dispatcher', 'collections/tags'], function(Marionette, template, K2Dispatcher, K2CollectionTags) {
 	var K2ViewItem = Marionette.ItemView.extend({
+		template : _.template(template),
+		modelEvents : {
+			'change' : 'render'
+		},
+		events : {
+			'keypress #tags' : 'searchTags'
+		},
 		initialize : function() {
 			K2Dispatcher.on('app:controller:beforeSave', function() {
 				this.onBeforeSave();
 			}, this);
-		},
-		template : _.template(template),
-		modelEvents : {
-			'change' : 'render'
+			this.tags = new K2CollectionTags();
 		},
 		serializeData : function() {
 			var data = {
@@ -17,12 +21,17 @@ define(['marionette', 'text!layouts/items/form.html', 'dispatcher'], function(Ma
 			};
 			return data;
 		},
+		searchTags : function(event) {
+			var el = jQuery(event.currentTarget);
+			this.tags.setState('search', el.val());
+			this.tags.fetch();
+		},
 		onBeforeSave : function() {
 			K2Editor.save('text');
 		},
 		onDomRefresh : function() {
 			K2Editor.init();
-			if (SqueezeBox !== undefined) {
+			if (typeof(SqueezeBox) !== 'undefined') {
 				SqueezeBox.initialize({});
 				SqueezeBox.assign($$('a.modal-button'), {
 					parse : 'rel'
