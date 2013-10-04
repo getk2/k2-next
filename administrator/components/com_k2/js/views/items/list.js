654587487779a15bd2ccdde0ce4175810c38cc63
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-10-04 20:18:36 +0300

    Added jQuery sortable plugin.

--- administrator/components/com_k2/js/views/items/list.js
+++ administrator/components/com_k2/js/views/items/list.js
@@ -14,7 +14,19 @@ define(['marionette', 'text!layouts/items/list.html', 'text!layouts/items/row.ht
 	var K2ViewItems = Marionette.CompositeView.extend({
 		template : _.template(list),
 		itemViewContainer : 'tbody',
-		itemView : K2ViewItemsRow
+		itemView : K2ViewItemsRow,
+		onDomRefresh : function() {
+			var el = this.$el.find('table');
+			require(['widgets/sortable/jquery-sortable-min', 'widgets/sortable/jquery-sortable-min'], function() {
+				console.info('aa');
+				el.sortable({
+					containerSelector : 'table',
+					itemPath : '> tbody',
+					itemSelector : 'tr',
+					placeholder : '<tr class="placeholder"/>'
+				});
+			})
+		}
 	});
 	return K2ViewItems;
 });
