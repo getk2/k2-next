e353b2fdbd0f2dab01d368e520d67863c3f3b99e
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-10-24 15:22:35 +0300

    Removed unused files.

--- administrator/components/com_k2/js/collections/attachments.js
+++
@@ -1,9 +0,0 @@
-define(['backbone', 'collection', 'models/attachments'], function(Backbone, K2Collection, K2ModelAttachments) {'use strict';
-	var K2CollectionAttachments = K2Collection.extend({
-		model : K2ModelAttachments,
-		url : function() {
-			return 'index.php?option=com_k2&task=attachments.sync&format=json' + this.buildQuery();
-		}
-	});
-	return K2CollectionAttachments;
-});
