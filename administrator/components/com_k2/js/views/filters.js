23ba8890523eb9d1d0e99bbbe0126509d0d51e7c
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-12-03 15:48:42 +0200

    Added author filter to items view.

--- administrator/components/com_k2/js/views/filters.js
+++ administrator/components/com_k2/js/views/filters.js
@@ -16,7 +16,7 @@ define(['marionette', 'text!layouts/filters.html', 'dispatcher', 'widgets/widget
 		initialize : function() {
 			
 			// Model
-			this.model = new Backbone.Model({filters : []});
+			this.model = new Backbone.Model({filters : [], states:[]});
 			
 			// Listener for updating filters
 			K2Dispatcher.on('app:update:subheader', function(response) {
@@ -57,13 +57,17 @@ define(['marionette', 'text!layouts/filters.html', 'dispatcher', 'widgets/widget
 			require(['widgets/select2/select2', 'css!widgets/select2/select2.css'], _.bind(function() {
 				this.$el.find('.appFilters select').select2();
 
-				//var states = this.model.get('states');
-				//this.$el.find('.appFilters input[name="author"]').select2('data', {
-				//	id : states.author,
-				//	text : states.authorName
-				//});
+				var states = this.model.get('states');
+				this.$el.find('.appFilters input[name="author"]').select2('data', {
+					id : states.author,
+					text : states.authorName
+				});
 
 			}, this));
+		},
+		
+		onDomRefresh : function() {
+			K2Widget.updateEvents(this.$el);
 		},
 
 		filter : function(event) {
