c493351f5a1c2011ce2feaccf073ef762f85b9bc
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-02-11 12:16:15 +0200

    Joomla! 2.5 compatibility updates.

--- administrator/components/com_k2/views/k2/view.html.php
+++ administrator/components/com_k2/views/k2/view.html.php
@@ -49,6 +49,7 @@ class K2ViewK2 extends JViewLegacy
 		$document->setMetaData('viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');
 
 		// Load jQuery
+		require_once JPATH_ADMINISTRATOR.'/components/com_k2/helpers/html.php';
 		K2HelperHTML::jQuery();
 
 		// Load the CSS
