67dc5698945c6f8d8d80df7ffce47898ed94d4ba
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-02-25 18:14:52 +0200

    Added first version of import articles function

--- administrator/components/com_k2/controllers/items.json.php
+++ administrator/components/com_k2/controllers/items.json.php
@@ -58,7 +58,7 @@ class K2ControllerItems extends K2Controller
 		}
 		return $data;
 	}
-	
+
 	public function close()
 	{
 		// Check for token
@@ -73,6 +73,180 @@ class K2ControllerItems extends K2Controller
 		}
 		$this->model->close();
 		return $this;
+	}
+
+	public function import()
+	{
+		// User
+		$application = JFactory::getApplication();
+		$user = JFactory::getUser();
+		$session = JFactory::getSession();
+		$db = JFactory::getDbo();
+		$id = $application->input->get('id', 0, 'int');
+
+		// Permissions check
+		if (!$user->authorise('core.admin'))
+		{
+			K2Response::throwError(JText::_('K2_YOU_ARE_NOT_AUTHORIZED_TO_PERFORM_THIS_OPERATION'), 403);
+		}
+
+		// Setup session variables
+		if ($id == 0)
+		{
+			$mapping = new stdClass;
+			$mapping->articles = array();
+			$mapping->categories = array();
+		}
+		else
+		{
+			$mapping = $session->get('k2.import.mapping');
+		}
+
+		$query = $db->getQuery(true);
+		$query->select('*')->from($db->quoteName('#__content'))->where($db->quoteName('id').' > '.$id)->order($db->quoteName('id'));
+		$db->setQuery($query, 0, 1);
+		$article = $db->loadObject();
+
+		if (!$article)
+		{
+			// @TODO: We are done. We need to fix categories relations and clear the session variables
+			return $this;
+		}
+
+		$itemData = array();
+		$itemData['id'] = null;
+		$itemData['title'] = $article->title;
+
+		// Detect category
+		if (isset($mapping->categories[$article->catid]))
+		{
+			$itemData['catid'] = $mapping->categories[$article->catid];
+		}
+		else
+		{
+			$query = $db->getQuery(true);
+			$query->select('*')->from($db->quoteName('#__categories'))->where($db->quoteName('id').' = '.$article->catid);
+			$db->setQuery($query, 0, 1);
+			$category = $db->loadObject();
+			$categoryData = array();
+			$categoryData['id'] = null;
+			$categoryData['title'] = $category->title;
+			$categoryData['description'] = $category->description;
+			if ($category->published < 0)
+			{
+				$categoryData['state'] = -1;
+			}
+			else if ($category->published > 0)
+			{
+				$categoryData['state'] = 1;
+			}
+			else
+			{
+				$categoryData['state'] = 0;
+			}
+			$categoryData['parent_id'] = 1;
+			$categoryData['access'] = $category->access;
+			$categoryData['language'] = $category->language;
+			$categoryParams = new JRegistry($category->params);
+			$categoryImage = $categoryParams->get('image');
+			if ($categoryImage)
+			{
+				$image = K2HelperImages::add('category', null, $categoryImage);
+				$categoryData['image'] = array('id' => '', 'temp' => $image->temp, 'path' => '', 'remove' => 0, 'caption' => '', 'credits' => '');
+			}
+
+			$model = K2Model::getInstance('Categories');
+			$model->setState('data', $categoryData);
+			$model->save();
+
+			$categoryId = $model->getState('id');
+
+			// Update date and author information since the model has auto set this data during save
+			$query = $db->getQuery(true);
+			$query->update($db->quoteName('#__k2_categories'));
+			$query->set($db->quoteName('created').' = '.$db->quote($category->created_time));
+			$query->set($db->quoteName('modified').' = '.$db->quote($article->modified_time));
+			$query->set($db->quoteName('created_by').' = '.$db->quote($article->created_user_id));
+			$query->set($db->quoteName('modified_by').' = '.$db->quote($article->modified_user_id));
+			$query->where($db->quoteName('id').' = '.$categoryId);
+			$db->setQuery($query);
+			$db->execute();
+
+			$itemData['catid'] = $categoryId;
+
+			$mapping->categories[$article->catid] = $categoryId;
+		}
+
+		if ($article->state < 0)
+		{
+			$itemData['state'] = -1;
+		}
+		else if ($article->state > 0)
+		{
+			$itemData['state'] = 1;
+		}
+		else
+		{
+			$itemData['state'] = 0;
+		}
+
+		// Detect featured state
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName('content_id'))->from($db->quoteName('#__content_frontpage'))->where($db->quoteName('content_id').' = '.$article->id);
+		$db->setQuery($query);
+		$featured = $db->loadResult();
+
+		$itemData['featured'] = $featured ? 1 : 0;
+		$itemData['introtext'] = $article->introtext;
+		$itemData['fulltext'] = $article->fulltext;
+		$itemData['created_by_alias'] = $article->created_by_alias;
+		$itemData['publish_up'] = $article->publish_up;
+		$itemData['publish_down'] = $article->publish_down;
+		$itemData['access'] = $article->access;
+		$itemData['ordering'] = $article->ordering;
+		$metadata = (array)json_decode($article->metadata);
+		$metadata['keywords'] = $article->metakey;
+		$metadata['description'] = $article->metadesc;
+		$itemData['metadata'] = json_encode($metadata);
+		$itemData['language'] = $article->language;
+		$itemData['tags'] = $article->metakey;
+
+		$articleImages = new JRegistry($article->images);
+
+		if ($articleImages->get('image_fulltext'))
+		{
+			$image = K2HelperImages::add('item', null, $articleImages->get('image_fulltext'));
+			$itemData['image'] = array('id' => '', 'temp' => $image->temp, 'path' => '', 'remove' => 0, 'caption' => $articleImages->get('image_fulltext_caption'), 'credits' => '');
+		}
+		else if ($articleImages->get('image_intro'))
+		{
+			$image = K2HelperImages::add('item', null, $articleImages->get('image_intro'));
+			$itemData['image'] = array('id' => '', 'temp' => $image->temp, 'path' => '', 'remove' => 0, 'caption' => $articleImages->get('image_intro_caption'), 'credits' => '');
+		}
+
+		$model = K2Model::getInstance('Items');
+		$model->setState('data', $itemData);
+		$model->save();
+
+		$itemId = $model->getState('id');
+
+		// Update date and author information since the model has auto set this data during save
+		$query = $db->getQuery(true);
+		$query->update($db->quoteName('#__k2_items'));
+		$query->set($db->quoteName('created').' = '.$db->quote($article->created));
+		$query->set($db->quoteName('modified').' = '.$db->quote($article->modified));
+		$query->set($db->quoteName('created_by').' = '.$db->quote($article->created_by));
+		$query->set($db->quoteName('modified_by').' = '.$db->quote($article->modified_by));
+		$query->where($db->quoteName('id').' = '.$itemId);
+		$db->setQuery($query);
+		$db->execute();
+
+		$mapping->articles[$article->id] = $itemId;
+
+		$session->set('k2.import.mapping', $mapping);
+
+		return $this;
+
 	}
 
 }
