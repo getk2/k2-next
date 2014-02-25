d0db2002fa5f0ed5b6748c8b08893df3b3aebe9f
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-02-25 15:59:04 +0200

    Added uninstall.sql file.

---
+++ administrator/components/com_k2/uninstall.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `#__k2_attachments`;
+DROP TABLE IF EXISTS `#__k2_categories`;
+DROP TABLE IF EXISTS `#__k2_comments`;
+DROP TABLE IF EXISTS `#__k2_extra_fields`;
+DROP TABLE IF EXISTS `#__k2_extra_fields_groups`;
+DROP TABLE IF EXISTS `#__k2_items`;
+DROP TABLE IF EXISTS `#__k2_items_stats`;
+DROP TABLE IF EXISTS `#__k2_revisions`;
+DROP TABLE IF EXISTS `#__k2_tags`;
+DROP TABLE IF EXISTS `#__k2_tags_xref`;
+DROP TABLE IF EXISTS `#__k2_users`;
+DROP TABLE IF EXISTS `#__k2_users_stats`;
