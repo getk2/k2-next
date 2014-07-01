b01839d6a1be1701b0eb4b36d2c185946a3718a4
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-07-01 18:45:51 +0300

    Updated revisions widget.

--- administrator/components/com_k2/js/views/revisions/widget.js
+++ administrator/components/com_k2/js/views/revisions/widget.js
@@ -1,16 +1,34 @@
 define(['text!layouts/revisions/form.html', 'dispatcher', 'jqueryui', 'widgets/sliderpips/jquery-ui-slider-pips.min', 'css!widgets/sliderpips/jquery-ui-slider-pips.css', 'css!//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/flick/jquery-ui.min.css'], function(template, K2Dispatcher) {'use strict';
 
+	// Model
+	var Revision = Backbone.Model.extend({
+		defaults : {
+			id : null,
+			date : null,
+			data : null,
+			user : null
+		}
+	});
+
+	// Collection
+	var Revisions = Backbone.Collection.extend({
+		model : Revision
+	});
+
 	var K2ViewRevisionsWidget = Marionette.ItemView.extend({
 		template : _.template(template),
 		initialize : function(options) {
-			this.revisions = options.data;
+			options.data.reverse();
+			this.collection = new Revisions(options.data);
+			_.each(this.collection.models, function(model) {
+				model.set('data', jQuery.parseJSON(model.get('data')));
+			});
 		},
 		onDomRefresh : function() {
-			
+
 			var el = this.$el.find('div[data-role="revisions-timeline"]');
-			var max = this.revisions.length;
-			
-			
+			var max = this.collection.models.length;
+
 			// First fix jQuery UI/Mootools conflict
 			el[0].slide = null;
 			// Triggger the slider
@@ -18,20 +36,24 @@ define(['text!layouts/revisions/form.html', 'dispatcher', 'jqueryui', 'widgets/s
 				min : 1,
 				max : max,
 				step : 1,
-				values : [max-1, max]
+				values : [max - 1, max]
 			});
-			
+
 			var labels = [];
-			_.each(this.revisions, function(revision) {
-				labels.push('<span>' + revision.user + '</span><span>' + revision.date + '</span>');
+			_.each(this.collection.models, function(revision) {
+				labels.push('<span>' + revision.get('user') + '</span><span>' + revision.get('date') + '</span>');
 			});
-			
-			el.slider("pips" , { rest: "label", labels: labels });
+
+			el.slider('pips', {
+				rest : 'label',
+				labels : labels
+			});
 		},
 		// Serialize data for view
 		serializeData : function() {
 			var data = {
-				'revisions' : this.revisions
+				'revisions' : this.collection,
+				'leftRevision' : this.collection.get(2)
 			};
 			return data;
 		}
