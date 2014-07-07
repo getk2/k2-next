25f6638ce605e034eedaf5e2d7260f4b4c7b32dd
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-07-07 15:15:14 +0300

    Updated utilities view.

--- administrator/components/com_k2/js/views/utilities.js
+++ administrator/components/com_k2/js/views/utilities.js
@@ -10,6 +10,7 @@ define(['marionette', 'text!layouts/utilities.html', 'dispatcher'], function(Mar
 		import : function(event) {
 			event.preventDefault();
 			if (confirm(l('K2_WARNING_YOU_ARE_ABOUT_TO_IMPORT_ALL_SECTIONS_CATEGORIES_AND_ARTICLES_FROM_JOOMLAS_CORE_CONTENT_COMPONENT_COM_CONTENT_INTO_K2_IF_THIS_IS_THE_FIRST_TIME_YOU_IMPORT_CONTENT_TO_K2_AND_YOUR_SITE_HAS_MORE_THAN_A_FEW_THOUSAND_ARTICLES_THE_PROCESS_MAY_TAKE_A_FEW_MINUTES_IF_YOU_HAVE_EXECUTED_THIS_OPERATION_BEFORE_DUPLICATE_CONTENT_MAY_BE_PRODUCED'))) {
+				jQuery('[data-action="import"]').prop('disabled', true);
 				this._import(0);
 			}
 		},
@@ -18,17 +19,27 @@ define(['marionette', 'text!layouts/utilities.html', 'dispatcher'], function(Mar
 			jQuery.post('index.php?option=com_k2&task=items.import&id=' + id + '&format=json', K2SessionToken + '=1', function(data) {
 				if (data && data.lastId) {
 					self._import(data.lastId);
+				} else {
+					jQuery('[data-action="import"]').prop('disabled', false);
 				}
 			});
 		},
 		deleteOrphanTags : function(event) {
 			event.preventDefault();
-			jQuery.post('index.php?option=com_k2&task=tags.deleteOrphans&format=json', K2SessionToken + '=1');
+			var button = jQuery(event.currentTarget);
+			button.prop('disabled', true);
+			jQuery.post('index.php?option=com_k2&task=tags.deleteOrphans&format=json', K2SessionToken + '=1', function() {
+				button.prop('disabled', false);
+			});
 		},
 		deleteUnpublishedComments : function(event) {
 			event.preventDefault();
+			var button = jQuery(event.currentTarget);
+			button.prop('disabled', true);
 			if (confirm(l('K2_THIS_WILL_PERMANENTLY_DELETE_ALL_UNPUBLISHED_COMMENTS_ARE_YOU_SURE'))) {
-				jQuery.post('index.php?option=com_k2&task=comments.deleteUnpublished&format=json', K2SessionToken + '=1');
+				jQuery.post('index.php?option=com_k2&task=comments.deleteUnpublished&format=json', K2SessionToken + '=1', function() {
+					button.prop('disabled', false);
+				});
 			}
 		}
 	});
