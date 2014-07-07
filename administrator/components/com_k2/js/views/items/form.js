c35825030605d3fe24c68cf630a3ead878f9a897
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-07-07 16:04:12 +0300

    Updated the item view to load revisions only when the tab is clicked.

--- administrator/components/com_k2/js/views/items/form.js
+++ administrator/components/com_k2/js/views/items/form.js
@@ -17,7 +17,8 @@ define(['dispatcher', 'widgets/widget', 'text!layouts/items/form.html', 'views/e
 
 		// UI events
 		events : {
-			'change #catid' : 'updateCategory'
+			'change #catid' : 'updateCategory',
+			'click ul[data-role="tabs-navigation"] li:nth-child(2) a' : 'loadRevisions'
 		},
 
 		modelEvents : {
@@ -63,28 +64,6 @@ define(['dispatcher', 'widgets/widget', 'text!layouts/items/form.html', 'views/e
 				resourceId : this.model.get('id'),
 				scope : 'item'
 			});
-
-			// Revisions
-			this.revisionsView = new K2ViewRevisionsWidget({
-				data : this.model.get('revisions'),
-				parent : this
-			});
-			this.revisionsView.on('restore', _.bind(function(revision) {
-				var data = revision.get('data');
-				this.$('input[name="title"]').val(data.title);
-				var form = this.model.getForm();
-				if (form.has('text')) {
-					var text = '';
-					text += data.introtext;
-					if (data.fulltext) {
-						text += '<hr id="system-readmore" />' + data.fulltext;
-					}
-					K2Editor.setContent('text', text);
-				} else {
-					K2Editor.setContent('introtext', data.introtext);
-					K2Editor.setContent('fulltext', data.fulltext);
-				}
-			}, this));
 
 		},
 
@@ -177,7 +156,30 @@ define(['dispatcher', 'widgets/widget', 'text!layouts/items/form.html', 'views/e
 			// Extra fields
 			this.extraFieldsRegion.show(this.extraFieldsView);
 
+		},
+
+		loadRevisions : function() {
 			// Revisions
+			this.revisionsView = new K2ViewRevisionsWidget({
+				data : this.model.get('revisions'),
+				parent : this
+			});
+			this.revisionsView.on('restore', _.bind(function(revision) {
+				var data = revision.get('data');
+				this.$('input[name="title"]').val(data.title);
+				var form = this.model.getForm();
+				if (form.has('text')) {
+					var text = '';
+					text += data.introtext;
+					if (data.fulltext) {
+						text += '<hr id="system-readmore" />' + data.fulltext;
+					}
+					K2Editor.setContent('text', text);
+				} else {
+					K2Editor.setContent('introtext', data.introtext);
+					K2Editor.setContent('fulltext', data.fulltext);
+				}
+			}, this));
 			this.revisionsRegion.show(this.revisionsView);
 		}
 	});
