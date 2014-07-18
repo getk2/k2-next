02a71311af6cb6285aff314758ff5fd55c8803b3
Krikor Boghossian <krikor.boghossian@gmail.com>
2014-07-18 20:45:28 +0300

    Fixed an issue in extra field definitions. Styling on sortable fields

--- administrator/components/com_k2/extrafields/textarea/definition.php
+++ administrator/components/com_k2/extrafields/textarea/definition.php
@@ -14,7 +14,11 @@ defined('_JEXEC') or die ; ?>
 	<input name="<?php echo $field->get('prefix'); ?>[rows]" type="text" value="<?php echo (int)$field->get('rows'); ?>" />
 	<label><?php echo JText::_('K2_COLUMNS'); ?></label>
 	<input name="<?php echo $field->get('prefix'); ?>[columns]" type="text" value="<?php echo (int)$field->get('columns'); ?>" />
+	
+	<label>
+		<input value="1" name="<?php echo $field->get('prefix'); ?>[editor]" type="checkbox" <?php if($field->get('editor')) { echo 'checked="checked"';} ?> />
+		<?php echo JText::_('K2_USE_EDITOR'); ?>
+	</label>
 	<div><textarea rows="10" cols="40" name="<?php echo $field->get('prefix'); ?>[value]"><?php echo $field->get('value'); ?></textarea></div>
-	<label><?php echo JText::_('K2_USE_EDITOR'); ?></label>
-	<input value="1" name="<?php echo $field->get('prefix'); ?>[editor]" type="checkbox" <?php if($field->get('editor')) { echo 'checked="checked"';} ?> />
+	
 </div>
