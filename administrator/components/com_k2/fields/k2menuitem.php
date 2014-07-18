14d4359c3193466170f62d04ec912a03c2e0ace9
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-07-18 13:04:31 +0300

    Added custom field for selecting the Advanced SEF menu links prefixes.

---
+++ administrator/components/com_k2/fields/k2menuitem.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @version		3.0.0
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die ;
+
+JFormHelper::loadFieldClass('menuitem');
+
+class JFormFieldK2Menuitem extends JFormFieldMenuitem
+{
+	public $type = 'K2MenuItem';
+
+	/**
+	 * Method to get the field option groups.
+	 *
+	 * @return  array  The field option objects as a nested array in groups.
+	 *
+	 * @since   1.6
+	 */
+	protected function getGroups()
+	{
+		$this->filterAttribute = (string)$this->element['filter_attribute'];
+		$this->filterValue = (string)$this->element['filter_value'];
+		$groups = parent::getGroups();
+		if ($this->filterAttribute && $this->filterValue)
+		{
+			$application = JApplication::getInstance('site');
+			$menu = $application->getMenu();
+			$items = $menu->getItems($this->filterAttribute, $this->filterValue);
+			$enabled = array();
+			foreach ($items as $item)
+			{
+				$enabled[] = $item->id;
+			}
+			foreach ($groups as $options)
+			{
+				foreach ($options as $option)
+				{
+					if (!in_array($option->value, $enabled))
+					{
+						$option->disable = true;
+					}
+				}
+			}
+		}
+		return $groups;
+	}
+
+}
