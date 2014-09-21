2570240ebc623e49e510082cfbdffd80cf45476a
Krikor Boghossian <krikor.boghossian@gmail.com>
2014-09-21 19:25:21 +0300

    Fixed module styling

--- administrator/components/com_k2/fields/tmpl/settings.php
+++ administrator/components/com_k2/fields/tmpl/settings.php
@@ -11,35 +11,55 @@
 defined('_JEXEC') or die ; ?>
 
 
-<div id="K2SettingsContainer">
+<div id="K2SettingsContainer" class="jw">
+
+
 	<div id="K2SettingsMenu">
-		<ul>
+		<ul class="jw--tabs jw--tabs__top">
 			<?php foreach ($this->form->getFieldsets() as $name => $fieldset): ?>				
 			<?php if($name != 'basic' && strpos($name, 'K2GROUP_') === false): ?>
-				<li><a href="#<?php echo $name; ?>"><?php echo JText::_($fieldset->label); ?></a></li>
+			<li><a href="#<?php echo $name; ?>"><?php echo JText::_($fieldset->label); ?></a></li>
 			<?php endif; ?>
 			<?php endforeach; ?>
 		</ul>
 	</div>
-	<div id="K2Settings">
-	<?php foreach ($this->form->getFieldsets() as $name => $fieldset): ?>
-	<?php if($name != 'basic'): ?>
+	
+	
+	<div id="K2Settings" class="jw--settings jw--generic--form">
+		<?php foreach ($this->form->getFieldsets() as $name => $fieldset): ?>
+		<?php if($name != 'basic'): ?>
 		<?php $attributes = strpos($name, 'K2GROUP_') === 0 ? 'style="display:none;" data-k2group="'.substr($name, 8).'"' : ''; ?>
-		<div class="K2SettingsSection" <?php echo $attributes;?>>
+		<div class="jw--setting--field K2SettingsSection" <?php echo $attributes;?>>
+		
 			<h3 class="K2SettingsSectionHeader" id="<?php echo $name; ?>"><?php echo JText::_($fieldset->label); ?></h3>
+			
 			<?php if (isset($fieldset->description) && !empty($fieldset->description)) : ?>
-				<p><?php echo JText::_($fieldset->description); ?></p>
+			<p><?php echo JText::_($fieldset->description); ?></p>
 			<?php endif; ?>
 
 			<?php foreach ($this->form->getFieldset($name) as $field): ?>
-				<div class="K2SettingsRow K2SettingsField<?php echo $field->type; ?>">
+			
+			<?php if($field->type == 'K2Header'): ?>
+			
+			<?php echo $field->getInput(); ?>
+			
+			<?php else: ?>
+			
+			<div class="jw--setting--single ov-hidden K2SettingsRow K2SettingsField<?php echo $field->type; ?>">
+				<div class="left">
 					<?php echo $field->getLabel(); ?>
+				</div>
+				<div class="right">
 					<?php echo $field->getInput(); ?>
 				</div>
-				<div class="clr"></div>
+			</div>
+			
+			<?php endif; ?>
 			<?php endforeach; ?>
 		</div>
-	<?php endif; ?>
-	<?php endforeach; ?>
+		<?php endif; ?>
+		<?php endforeach; ?>
+	
+	
 	</div>
 </div>
