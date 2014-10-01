8272b7542ade128a932b43cd54159964b4ceacbe
Lefteris Kavadas (Office) <lefteris@nuevvo.com>
2014-10-01 16:41:00 +0300

    Added basic definition of extra field header to allow upgrading of such fields from version 2.

---
+++ administrator/components/com_k2/extrafields/header/output.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @version		3.0.0b
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die ; ?>
