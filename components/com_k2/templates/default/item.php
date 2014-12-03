d87d15f4302597c0e30ce4b6139a124835b6bfb7
Lefteris Kavadas <lefteris@nuevvo.com>
2014-12-03 15:37:58 +0200

    Removed the option to select the comments position ( above or bellow comments ) as requested. Moved the code from item_comments_form.php to item.php as requested.

--- components/com_k2/templates/default/item.php
+++ components/com_k2/templates/default/item.php
@@ -504,11 +504,6 @@ defined('_JEXEC') or die ;
   
 	<script type="text/template" id="k2CommentsTemplate">
 		<div class="itemComments">
-
-			<?php if($this->params->get('commentsFormPosition')=='above' && !$this->print && $this->user->canComment): ?>
-			<!-- Item comments form -->
-			<div class="itemCommentsForm"><?php echo $this->loadTemplate('comments_form'); ?></div>
-			<?php endif; ?>
 	
 			<!-- Item comments -->
 			<% if(comments.length) { %>
@@ -601,9 +596,54 @@ defined('_JEXEC') or die ;
 
 			<% } %>
 			
-			<?php if($this->params->get('commentsFormPosition')=='below' && !$this->print && $this->user->canComment): ?>
+			<?php if(!$this->print && $this->user->canComment): ?>
 			<!-- Item comments form -->
-			<div class="itemCommentsForm"><?php echo $this->loadTemplate('comments_form'); ?></div>
+			<div class="itemCommentsForm">
+				<h3><?php echo JText::_('K2_LEAVE_A_COMMENT') ?></h3>
+
+				<?php if($this->params->get('commentsFormNotes')): ?>
+				<p class="itemCommentsFormNotes">
+					<?php if($this->params->get('commentsFormNotesText')): ?>
+					<?php echo nl2br($this->params->get('commentsFormNotesText')); ?>
+					<?php else: ?>
+					<?php echo JText::_('K2_COMMENT_FORM_NOTES') ?>
+					<?php endif; ?>
+				</p>
+				<?php endif; ?>
+				
+				<form action="<?php echo JRoute::_('index.php'); ?>" method="post" data-form="comments">
+					<label for="k2CommentText"><?php echo JText::_('K2_MESSAGE'); ?> *</label>
+					<textarea rows="10" cols="20" placeholder="<?php echo JText::_('K2_ENTER_YOUR_MESSAGE_HERE'); ?>" name="text" id="k2CommentText"></textarea>
+					
+					<label for="k2CommentName"><?php echo JText::_('K2_NAME'); ?> *</label>
+					
+					<?php if($this->user->guest): ?>
+					<input type="text" name="name" id="k2CommentName" placeholder="<?php echo JText::_('K2_ENTER_YOUR_NAME'); ?>" />
+					<?php else : ?>
+					<input type="text" name="name" id="k2CommentName" value="<?php echo htmlspecialchars($this->user->name); ?>" readonly="readonly" />
+					<?php endif; ?>
+					
+					<label for="k2CommentEmail"><?php echo JText::_('K2_EMAIL'); ?> *</label>
+					
+					<?php if($this->user->guest): ?>
+					<input type="email" name="email" id="k2CommentEmail" placeholder="<?php echo JText::_('K2_ENTER_YOUR_EMAIL_ADDRESS'); ?>"  />
+					<?php else : ?>
+					<input type="email" name="email" id="k2CommentEmail" value="<?php echo htmlspecialchars($this->user->email); ?>" readonly="readonly" />
+					<?php endif; ?>
+					
+					<label for="k2CommentUrl"><?php echo JText::_('K2_WEBSITE_URL'); ?></label>
+					<input type="text" name="url" id="k2CommentUrl" placeholder="<?php echo JText::_('K2_ENTER_YOUR_SITE_URL'); ?>" />
+				
+					<?php echo K2HelperCaptcha::display(); ?>
+				
+					<button type="submit" data-action="create"><?php echo JText::_('K2_SUBMIT_COMMENT'); ?></button>
+					
+					<input type="hidden" name="itemId" value="<?php echo $this->item->id; ?>" />
+					
+					<span data-role="log"></span>
+					
+				</form>
+			</div>
 			<?php endif; ?>
 	
 			<?php if (!$this->user->canComment && $this->user->guest): ?>
