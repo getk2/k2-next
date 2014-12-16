549f24cdaa4037126635987da8bd93633bb54f6d
Lefteris Kavadas (Office) <lefteris@nuevvo.com>
2014-12-16 17:40:30 +0200

    Fixed frontend editing toolbar issue.

--- plugins/system/k2/k2.php
+++ plugins/system/k2/k2.php
@@ -369,7 +369,7 @@ class PlgSystemK2 extends JPlugin
 		$view = $application->input->get('view');
 		
 		// Add user toolbar
-		if($application->isSite() && $document->getType() == 'html' && $user->authorise('core.manage', 'com_k2') && $view != 'admin')
+		if($application->isSite() && $document->getType() == 'html' && $user->authorise('core.manage', 'com_k2') && $view != 'admin' && $view != 'k2')
 		{
 			$author = K2Users::getInstance($user->id);
 			ob_start();
