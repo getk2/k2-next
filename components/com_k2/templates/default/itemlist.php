8c58dfeed8bbe6772565d5e289aaa1c7ee268e0d
Krikor Boghossian <krikor@nuevvo.com>
2015-08-28 19:25:37 +0300

    added a blank file

---
+++ components/com_k2/templates/default/itemlist.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @version		3.0.0
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2015 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die ; ?>
