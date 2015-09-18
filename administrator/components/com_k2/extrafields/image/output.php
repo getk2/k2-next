a65c444b3cfc6d0579b884f23eed308368b1981e
Lefteris Kavadas <lefteris@nuevvo.com>
2015-09-18 11:46:02 +0300

    Fixed #243

--- administrator/components/com_k2/extrafields/image/output.php
+++ administrator/components/com_k2/extrafields/image/output.php
@@ -11,5 +11,5 @@
 defined('_JEXEC') or die ; ?>
 
 <?php if($field->get('src')): ?>
-<img src="<?php echo JURI::root(true).htmlspecialchars($field->get('src'), ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($field->get('alt'), ENT_QUOTES, 'UTF-8'); ?>" />
+<img src="<?php echo JURI::root(true).'/'.htmlspecialchars($field->get('src'), ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($field->get('alt'), ENT_QUOTES, 'UTF-8'); ?>" />
 <?php endif; ?>
