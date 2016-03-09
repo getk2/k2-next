f554e49b3d4adae5fcfff376aff3c71825416481
Paul van Haren <paul.van.haren@kaleidoscoop-web.nl>
2016-03-09 21:28:51 +0100

    Implements switch to disable frontend toolbar
    Fixes #163

--- components/com_k2/templates/default/toolbar.php
+++ components/com_k2/templates/default/toolbar.php
@@ -11,21 +11,22 @@
 defined('_JEXEC') or die;
 
 ?>
-
-<div id="k2UserToolbar" data-role="k2-toolbar">
-	<div id="k2UserToolbarInnerContainer">
-		<?php if($author->image): ?>
-		<img src="<?php echo $author->image; ?>" alt="<?php echo htmlspecialchars($author->name, ENT_QUOTES, 'UTF-8'); ?>" />
-		<?php endif; ?>
-		<span><?php echo $author->name; ?></span>
-		<a data-role="k2-admin-link" href="<?php echo JURI::root(true); ?>/index.php?option=com_k2&view=admin">
-			<?php echo JText::_('K2_MANAGE_YOUR_CONTENT'); ?>
-		</a>
-		<?php if($params->get('inlineEditing')) : ?>
-			<div id="k2InlineEditControls">
-				<input id="save" type="submit" name="Save" class="btn btn-primary" value="<?php echo JText::_('K2_SAVE'); ?>">
-				<input id="cancel" type="submit" name="Cancel" class="btn btn-primary" value="<?php echo JText::_('K2_CANCEL'); ?>">
-			</div>
-		<?php endif;?>
+<?php if($params->get('frontendToolbar') != 0) : ?>
+	<div id="k2UserToolbar" data-role="k2-toolbar">
+		<div id="k2UserToolbarInnerContainer">
+			<?php if($author->image): ?>
+			<img src="<?php echo $author->image; ?>" alt="<?php echo htmlspecialchars($author->name, ENT_QUOTES, 'UTF-8'); ?>" />
+			<?php endif; ?>
+			<span><?php echo $author->name; ?></span>
+			<a data-role="k2-admin-link" href="<?php echo JURI::root(true); ?>/index.php?option=com_k2&view=admin">
+				<?php echo JText::_('K2_MANAGE_YOUR_CONTENT'); ?>
+			</a>
+			<?php if($params->get('inlineEditing')) : ?>
+				<div id="k2InlineEditControls">
+					<input id="save" type="submit" name="Save" class="btn btn-primary" value="<?php echo JText::_('K2_SAVE'); ?>">
+					<input id="cancel" type="submit" name="Cancel" class="btn btn-primary" value="<?php echo JText::_('K2_CANCEL'); ?>">
+				</div>
+			<?php endif;?>
+		</div>
 	</div>
-</div>
+<?php endif;?>
